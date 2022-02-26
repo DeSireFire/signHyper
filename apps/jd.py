27c5114edb7f9fe0fda789373765ee5df1dc76ea
羿潇贤鱼 <1025212779@qq.com>
2022-02-27 00:09:46 +0800

    fix cookie encode bug

--- apps/jd.py
+++ apps/jd.py
@@ -2,6 +2,8 @@
 JD 应用模块
 处理有关业务
 """
+from urllib.parse import quote
+
 from pydantic import BaseModel, Field
 from typing import Optional, Set
 from fastapi import FastAPI, Request, Body
@@ -37,6 +39,7 @@ async def set_jd_cookies(response: Response, item: post_data_item):
     print(f'pt_key,pt_pin--->{pt_key},{pt_pin}')
     res = None
     msg = "未知错误！联系管理员查看日志"
+    user_name = quote(f'网络的旅行者', 'utf-8')
     if pt_key and pt_pin:
         # 搜索变量
         datas = ql.envs_read(pt_pin) or []
@@ -49,14 +52,17 @@ async def set_jd_cookies(response: Response, item: post_data_item):
             res = ql.envs_create("JD_COOKIE", f"pt_key={pt_key};pt_pin={pt_pin};", f"{remark}")
             print(f"JD_COOKIE_NEW--->{res}")
         if res:
-            response.set_cookie(key="jd-user", value=f'{res.get("remarks")}')
+            response.set_cookie(key="jd-user", value=user_name)
+            user_name = quote(res.get("remarks"), 'utf-8')
+            response.set_cookie(key="jd-user", value=f'{user_name}')
             response.set_cookie(key="jd-id", value=f'{res.get("id")}')
             msg = "ok!"
             return {"status": 200, "msg": f"{msg}"}
     else:
         msg = "解析cookies时发生了错误！为取到关键参数！"
         return {"status": 0, "msg": f"{msg}"}
-    response.set_cookie(key="jd-user", value=f'网络的旅行者')
+
+    response.set_cookie(key="jd-user", value=user_name)
     return {"status": 0, "msg": f"{msg}"}
 
 # if __name__ == '__main__':
